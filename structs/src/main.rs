e60f3d00c83ac5d26eccd00d14b303d0b4d3045f
Rajbir Bhattacharjee <rajbir.bhattacharjee@gmail.com>
2021-06-27 06:27:19 -0700

    structs

---
+++ structs/src/main.rs
@@ -0,0 +1,63 @@
+struct User
+{
+    username:       String,
+    email:          String,
+    sign_in_count:  u64,
+    active:         bool
+}
+
+fn build_user(username: String, email: String) -> User
+{
+    let user = User {
+        username:       username,
+        email:          email,
+        sign_in_count:  0,
+        active:         true
+    };
+
+    return user;
+}
+
+fn build_user_2(username: String, email: String) -> User
+{
+    // Can omit field names
+    User {
+        username,
+        email,
+        sign_in_count:  0,
+        active:         false
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    let user1 = User {
+        email:          String::from("rajbir"),
+        username:       String::from("rajbir@example.com"),
+        sign_in_count:  0,
+        active:         true
+    };
+
+    let user2 = build_user(String::from("user2"), String::from("user2@example.com"));
+    let mut user2 = build_user_2(String::from("user2"), String::from("user2@example.com"));
+
+    user2.email = String::from("Some noew value");
+
+    let user3 = User
+    {
+        email:          String::from("another"),
+        username:       String::from("user@example.com"),
+        ..user2         // Fill the rest from user2
+    };
+
+    // Tuple Structs
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+    /*
+     * This will fail because they are of different types
+     * origin = black
+     */
+}
